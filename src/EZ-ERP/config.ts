625fd143ca9cdc388043588ea312ecc08803af9e
Wenjie Liu <wenjieliu@Wenjies-Mac-mini.local>
2025-04-20 20:54:20 -0700

    env

--- src/EZ-ERP/config.ts
+++ src/EZ-ERP/config.ts
@@ -1,3 +1,3 @@
 // API configuration
-//export const API_URL = 'https://ez-erp-backend.onrender.com/api' || 'http://localhost:3000/api';
-export const API_URL = 'http://localhost:3000/api';
+export const API_URL = 'https://ez-erp-backend.onrender.com/api';
+//export const API_URL = 'http://localhost:3000/api';
